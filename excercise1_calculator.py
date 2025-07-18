26cc22973a0ef728cfbaa7941aee558feaf33889
Aayush60-del <negiaayush2024@gmail.com>
2025-07-19 00:08:02 +0530

    Update excercise1_calculator.py

--- excercise1_calculator.py
+++ excercise1_calculator.py
@@ -1,1 +1,21 @@
 print("Online Calculator")
+a = int(input("type first number"))
+b = int(input("type second number"))
+
+add = a + b
+print("Addition of", a ,"+", b , "=" , add)
+
+mul = a*b
+print("Multiplication of", a , "*", b , "=" , mul)
+
+div = a % b 
+print("Division of", a , "/", b , "=" , div)
+
+expo = a**b 
+print("exponential of", a , "**" , b , "=" , expo)
+
+sub = a - b
+print("subtraction of " , a , "-" , b , "=" , sub)
+
+
+
